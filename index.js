b00e5f317b6915f14b77ea7b192ac1f7f4c122e7
Mezhgan Saadat <mezhgan@mezhgans-air.mynetworksettings.com>
2024-11-19 21:24:18 -0500

    first commit

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-// Given an array of integers, return an array such that each element
-// at index i is the product of all the numbers in the original
-// array except the one at i.
-// for solving this code problem let me go through each step
-// of course we need function with one parameter
-function returnInt(arr) {
-  // we need to return an array of numbers so we need to store them in a new array
-  let result = [];
-  // to go through each number we need for loop
-  for (let i = 0; i < arr.length; i++) {
-    // to get each number inside the loop we need to have a inner loop
-    // we need to store number 1 in a variable
-    // this one store the accumulation after each iteration meaning when we multiply two numbers it store the result of it
-    // this result is again multiplying with the second number and it continues until end of loop
-    let product = 1;
-    for (let j = 0; j < arr.length; j++) {
-      if (i !== j) {
-        // this ensures that current index i of outer loop is skipped
-        // meaning the index i should be skipped
-        product *= arr[j];
-        // product start at 1
-      }
-    }
-    result.push(product);
-    // push the result to result
-  }
-  return result;
-  // return the result
+// adding space between characters in a string by an index
+// to solve this coding problem :
+// there are two parameters in a function , first is the string and second is the index
+// (split) and (reduce) method and (if condition) are using for this code challenge
+// first split the string and with reduce method , the accumulator, current value and i = index are used
+// inside the (if condition) if i is bigger than 0 and i % the index of parameter of function is equal 0
+// " " space is adding to accumulator
+// with the accumulator, the current value adds
+// example "hello" with index of 2 means after each two characters there is a space
+// i         character      condition(i % 2 === 0)   updated accumulator
+// 0         h               false                    h
+// the condition is false because 0 % 2 is not equal 0 so no space
+// 1         e               false                    he
+// 2         l               true                     he l
+// 3         l               false                    he ll
+// 4         0               true                     he ll o
+function addSpaceBetweenChar(s, index){
+    return s.split("").reduce((acu, cur, i)=>{
+        if(i > 0 && i % index === 0){
+            acu += " "
+        }
+       return  acu + cur;
+    }, "");
 }
 
-console.log(returnInt([1,2,3,4,5]));
+console.log(addSpaceBetweenChar("hello", 2));
